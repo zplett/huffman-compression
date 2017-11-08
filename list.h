c937789174305e67aaa96284e3740239a3156b36
Zac Plett <zplett@clyde>
2017-11-08 16:21:58 -0500

    added init, get and insert functions to header file as well as commenting list.c

--- list.h
+++ list.h
@@ -14,3 +14,29 @@ typedef struct linked_list_s Linked_List;
 #define NODE_TYPE
 typedef enum node_type { INTERNAL, LEAF } node_type;
 #endif
+
+#ifndef GET
+#define GET
+Iterator* get( Linked_List *list, int index ); 
+#endif
+
+#ifndef TREE_NODE_INIT
+#define TREE_NODE_INIT
+Tree_Node* init_tree_node();
+#endif
+
+#ifndef LIST_NODE_INIT
+#define LIST_NODE_INIT
+List_Node* init_list_node();
+#endif
+
+#ifndef LINKED_LIST_INIT
+#define LINKED_LIST_INIT
+Linked_List* init_list();
+#endif
+
+#ifndef INSERT
+#define INSERT
+Iterator* insert( Linked_List list, int index );
+#endif
+
